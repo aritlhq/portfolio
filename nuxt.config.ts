1847afbdf0156ef16e20efaad09bbf2849e69417
aritlhq <aritlhq@gmail.com>
2025-06-13 10:16:23 +0700

    feat: add nuxt.config.ts with Naive UI and content module configuration

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    content: {
+        watch: {
+            enabled: true,
+        },
+    },
+    modules: ['@nuxt/content', 'nuxtjs-naive-ui'],
+    vite: {
+        plugins: [
+            AutoImport({
+                imports: [
+                    {
+                        'naive-ui': [
+                            'useDialog',
+                            'useMessage',
+                            'useNotification',
+                            'useLoadingBar'
+                        ]
+                    }
+                ]
+            }),
+            Components({
+                resolvers: [NaiveUiResolver()]
+            })
+        ]
+    },
+})

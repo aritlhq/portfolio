cd5bf333f44f973bcb5a9c3a9307002aa8b07f6f
aritlhq <aritlhq@gmail.com>
2025-06-13 10:16:41 +0700

    feat: add blog index page with post listing
    Add initial blog index page that displays all blog posts in descending date order. The page uses Nuxt's useAsyncData to fetch posts and renders them with title and description.

---
+++ pages/blog/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const {data: allPosts} = await useAsyncData('blog', () => queryCollection('blog').order('date', 'DESC').all())
+</script>
+
+<template>
+  <div v-for="post in allPosts" :key="post.id">
+    <NuxtLink :to="post.path">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.description }}</p>
+    </NuxtLink>
+  </div>
+</template>

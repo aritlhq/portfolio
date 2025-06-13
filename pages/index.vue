85037ba2dd988919e717ddc2ce89f5a7f84cd50b
aritlhq <aritlhq@gmail.com>
2025-06-13 10:16:31 +0700

    feat: add index page with SEO meta configuration

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+if (import.meta.server) {
+  /**
+   * Reference: https://nuxt.com/docs/api/composables/use-seo-meta
+   */
+  // These meta tags will only be added during server-side rendering
+  useSeoMeta({
+    robots: 'index, follow',
+    description: 'Static description that does not need reactivity',
+    ogImage: 'https://example.com/image.png',
+  })
+}
+
+const dynamicTitle = ref('My title')
+/**
+ * Reference: https://nuxt.com/docs/api/composables/use-seo-meta
+ */
+// Only use reactive meta tags outside the condition when necessary
+useSeoMeta({
+  title: () => dynamicTitle.value,
+  ogTitle: () => dynamicTitle.value,
+})
+</script>
+
+<template>
+  <h1>Hello</h1>
+</template>

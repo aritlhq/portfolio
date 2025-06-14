a6f78ec60dc1081a5475e2776a2c0bd673507943
aritlhq <aritlhq@gmail.com>
2025-06-14 09:43:35 +0700

    feat: add dark theme and global styling to app.vue
    Implemented dark theme using naive-ui and added global styling with font support. Wrapped content in layout container with padding.

--- app.vue
+++ app.vue
@@ -1,6 +1,22 @@
+<script setup>
+import {NConfigProvider, darkTheme} from 'naive-ui'
+import "@fontsource/montserrat/index.css"
+
+
+useHead({
+  titleTemplate: 'Faqih Alharits - %s',
+})
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtRouteAnnouncer/>
-    <NuxtPage/>
-  </NuxtLayout>
+  <NConfigProvider :theme="darkTheme">
+    <n-global-style/>
+    <div class="font-sans">
+      <NuxtLayout>
+        <div class="px-6">
+          <NuxtPage/>
+        </div>
+      </NuxtLayout>
+    </div>
+  </NConfigProvider>
 </template>

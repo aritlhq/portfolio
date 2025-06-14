470e070b23bb7f534a91623ab89686f8415600b6
aritlhq <aritlhq@gmail.com>
2025-06-14 09:43:26 +0700

    feat: add SEO meta tags to blog post page

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,11 +1,16 @@
 <script setup>
 const slug = useRoute().params.slug
-const { data: post } = await useAsyncData(`blog-${slug}`, () => {
-  return  queryCollection('blog').path(`/blog/${slug}`).first()
+const {data: post} = await useAsyncData(`blog-${slug}`, () => {
+  return queryCollection('blog').path(`/blog/${slug}`).first()
+});
+
+useSeoMeta({
+  title: post.value?.title,
+  description: post.value?.description
 })
 </script>
 
 <template>
   <!-- Render the blog post as Prose & Vue components -->
-  <ContentRenderer :value="post" />
+  <ContentRenderer :value="post"/>
 </template>

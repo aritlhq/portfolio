ea308b9b3946aa12f0957218308d4d873b25afeb
aritlhq <aritlhq@gmail.com>
2025-06-14 09:48:18 +0700

    feat: add Tabs component with naive-ui integration

---
+++ components/content/Tabs.vue
@@ -0,0 +1,23 @@
+<template>
+  <NTabs default-value="oasis">
+    <template #prefix>
+      Prefix
+    </template>
+    <NTabPane name="oasis" tab="Oasis">
+      <slot name="oasis"/>
+    </NTabPane>
+    <NTabPane name="the beatles" tab="the Beatles">
+      <slot/>
+    </NTabPane>
+    <NTabPane name="jay chou" tab="Jay Chou">
+      <slot/>
+    </NTabPane>
+    <template #suffix>
+      <slot/>
+    </template>
+  </NTabs>
+</template>
+
+<script>
+import {NTabs, NTabPane} from 'naive-ui';
+</script>

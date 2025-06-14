3f52e4b102590b8f4aff4f684dcbfefff2e46d51
aritlhq <aritlhq@gmail.com>
2025-06-14 09:47:03 +0700

    Add AutoImport for components and transpile naive-ui, vueuc with @unocss/nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,23 @@
-import AutoImport from 'unplugin-auto-import/vite'
 import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-05-15',
     devtools: {enabled: true},
+    build: {
+        /**
+         * Reference: https://github.com/tusen-ai/naive-ui/issues/6682#issuecomment-2688625916
+         */
+        transpile: ['naive-ui', 'vueuc']
+    },
     content: {
         watch: {
             enabled: true,
         },
     },
-    modules: ['@nuxt/content', 'nuxtjs-naive-ui'],
+    modules: ['@nuxt/content', 'nuxtjs-naive-ui', '@unocss/nuxt'],
     vite: {
         plugins: [
             AutoImport({
@@ -21,7 +27,8 @@ export default defineNuxtConfig({
                             'useDialog',
                             'useMessage',
                             'useNotification',
-                            'useLoadingBar'
+                            'useLoadingBar',
+                            'useCard'
                         ]
                     }
                 ]

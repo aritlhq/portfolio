dbad23b93ac8dbe090a4dc215a6e44d604dbaf22
aritlhq <aritlhq@gmail.com>
2025-06-19 15:55:52 +0700

    feat: add HomeView component with landing page sections

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import WelcomeMessage from "../components/landing-page/WelcomeMessage.vue";
+import Programming from "../components/landing-page/Programming.vue";
+import Educations from "../components/landing-page/Educations.vue";
+import Certificates from "../components/landing-page/Certificates.vue";
+</script>
+
+<template>
+  <div class="py-15">
+    <!--Welcome Message-->
+    <WelcomeMessage/>
+
+    <!--Programming-->
+    <Programming/>
+
+    <!--Educations-->
+    <Educations/>
+
+    <!--Educations-->
+    <Certificates/>
+  </div>
+</template>

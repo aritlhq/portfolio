99683cfaf89eb029396c1d96e7917e2d5b6843e4
aritlhq <aritlhq@gmail.com>
2025-06-20 01:36:52 +0700

    feat: display full URL in ProjectsView
    Added script to capture and display current full URL in ProjectsView component.

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,12 @@
+<script setup>
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+const fullUrl = window.location.origin + route.fullPath;
+</script>
+
 <template>
   <div>
-    Ini adalah halaman Projects
+    <p>Current URL: {{ fullUrl }}</p>
   </div>
 </template>

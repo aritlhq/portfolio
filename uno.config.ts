e9f70e8c46853e3539fd6782965bfd0dc685ec4e
aritlhq <aritlhq@gmail.com>
2025-06-19 15:53:57 +0700

    feat: add UnoCSS config with custom font families

---
+++ uno.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig} from 'unocss'
+
+export default defineConfig({
+    theme: {
+        fontFamily: {
+            comfortaa: '"Comfortaa", sans-serif',
+            anton: '"Anton", sans-serif',
+            caprasimo: '"Caprasimo", cursive',
+        },
+    },
+})

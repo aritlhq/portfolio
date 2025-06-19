238d57142b5d424637b47cd9bda9a7e5738dfddb
aritlhq <aritlhq@gmail.com>
2025-06-19 22:25:43 +0700

    style: reduce footer bottom padding from pb-20 to pb-10
    The change adjusts the footer's visual spacing by reducing the bottom padding while maintaining all other styling properties.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border-t pt-6 pb-20 px-4 text-center text-sm text-gray-500 dark:text-gray-400">
+  <div class="border-t pt-6 pb-10 px-4 text-center text-sm text-gray-500 dark:text-gray-400">
     <div class="mt-4">
       &copy; {{ new Date().getFullYear() }} Muhammad Faqih Alharits. All rights reserved.
     </div>

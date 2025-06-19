b08c33f439601d9ad88769b8ca23ad314f1b45ef
aritlhq <aritlhq@gmail.com>
2025-06-20 06:55:28 +0700

    refactor: wrap timeline component in Educations.vue with div
    The timeline component is now wrapped in a div for better styling control.

--- src/components/landing-page/Educations.vue
+++ src/components/landing-page/Educations.vue
@@ -9,23 +9,27 @@ import DotCircleRegular from "../../assets/icones/DotCircleRegular.vue";
       :key="category.title"
   >
     <h2 class="text-md lg:text-2xl">{{ category.title }}</h2>
-    <NTimeline size="large">
-      <NTimelineItem
-          v-for="(item, index) in category.data"
-          :key="index"
-          :title="item.title"
-          :content="item.content"
-          :time="item.time"
-          :type="item.type || undefined"
-          :line-type="item['line-type'] || undefined"
-          :color="item.type === 'success' ? '#f3ecd5' : undefined"
-      >
-        <template v-if="item.type ==='success'" #icon>
-          <n-icon>
-            <DotCircleRegular/>
-          </n-icon>
-        </template>
-      </NTimelineItem>
-    </NTimeline>
+
+    <!-- Tambahkan wrapper di sini -->
+    <div class="timeline-wrapper">
+      <NTimeline size="large">
+        <NTimelineItem
+            v-for="(item, index) in category.data"
+            :key="index"
+            :title="item.title"
+            :content="item.content"
+            :time="item.time"
+            :type="item.type || undefined"
+            :line-type="item['line-type'] || undefined"
+            :color="item.type === 'success' ? '#f3ecd5' : undefined"
+        >
+          <template v-if="item.type === 'success'" #icon>
+            <n-icon>
+              <DotCircleRegular/>
+            </n-icon>
+          </template>
+        </NTimelineItem>
+      </NTimeline>
+    </div>
   </div>
 </template>

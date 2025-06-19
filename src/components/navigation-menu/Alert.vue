89bdfa57d8b45502bfc4ad3b714fdedc3a4be97f
aritlhq <aritlhq@gmail.com>
2025-06-19 15:58:05 +0700

    feat: add development alert component to navigation menu

---
+++ src/components/navigation-menu/Alert.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AlertCirle from "../../assets/icones/AlertCirle.vue";
+</script>
+
+<template>
+  <div>
+    <n-alert class="bg-orange text-white" :show-icon="false" closable>
+      <n-marquee>
+        <div class="flex items-center gap-2">
+          <NIcon class="text-2xl">
+            <AlertCirle/>
+          </NIcon>
+          <div>
+            This website is still under development.
+          </div>
+        </div>
+      </n-marquee>
+    </n-alert>
+  </div>
+</template>

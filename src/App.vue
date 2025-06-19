8a0b99ba133390a545c1b650adbbaeaafcdb86f5
aritlhq <aritlhq@gmail.com>
2025-06-19 15:57:51 +0700

    feat: add dark theme and app layout with Poppins font
    Added dark theme configuration using naive-ui, implemented AppLayout component, and imported Poppins font for global styling.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {darkTheme} from 'naive-ui';
+import AppLayout from "./layouts/AppLayout.vue";
+</script>
+
+<template>
+  <NConfigProvider :theme="darkTheme">
+    <NGlobalStyle/>
+    <div class="font-popp">
+      <AppLayout>
+        <RouterView/>
+      </AppLayout>
+    </div>
+  </NConfigProvider>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Anton&family=Caprasimo&family=Comfortaa:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+.font-popp {
+  font-family: 'Poppins', sans-serif;
+}
+</style>

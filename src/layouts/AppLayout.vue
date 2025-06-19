8faa8327adc9283c2c10fcf8b9b5a5d1957aa110
aritlhq <aritlhq@gmail.com>
2025-06-19 15:57:44 +0700

    feat: add AppLayout component with navigation and footer
    The new AppLayout component provides a consistent layout structure with navigation menu and footer, using a responsive design with different padding sizes for various screen widths.

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import NavigationMenu from "../components/NavigationMenu.vue";
+import Footer from "../components/Footer.vue";
+</script>
+
+<template>
+  <div class="flex justify-between flex-col min-h-screen">
+    <NavigationMenu/>
+    <div class="px-4 py-3 md:px-20 lg:px-72">
+      <div>
+        <slot/>
+      </div>
+    </div>
+    <Footer/>
+  </div>
+</template>

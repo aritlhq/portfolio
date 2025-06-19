72b20a23381e25327649173c799c1c823273b4a8
aritlhq <aritlhq@gmail.com>
2025-06-20 01:37:50 +0700

    feat: enhance AboutView with new components and layout
    Add AvatarLarge and MyJourney components to AboutView with improved styling

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import AvatarLarge from "../components/about/AvatarLarge.vue";
+import MyJourney from "../components/about/MyJourney.vue";
+</script>
+
 <template>
   <div>
-    Ini adalah halaman about
+    <div class="justify-center flex w-auto">
+      <div class="items-center flex flex-col">
+        <AvatarLarge/>
+        <h1>About Me</h1>
+      </div>
+    </div>
+    <div class="mt-10">
+      <MyJourney/>
+    </div>
   </div>
 </template>

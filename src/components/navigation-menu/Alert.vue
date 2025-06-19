338d0d74a28a0943cd6f88a0406667bc51deb45d
aritlhq <aritlhq@gmail.com>
2025-06-19 22:26:44 +0700

    style: update Alert component styling and markup
    Changed alert background color and text color, added custom styling for close button, and improved markup structure.

--- src/components/navigation-menu/Alert.vue
+++ src/components/navigation-menu/Alert.vue
@@ -4,17 +4,23 @@ import AlertCirle from "../../assets/icones/AlertCirle.vue";
 
 <template>
   <div>
-    <n-alert class="bg-orange text-white" :show-icon="false" closable>
+    <n-alert class="custom-close-button-alert bg-[#f3ecd5]" :show-icon="false" closable>
       <n-marquee>
-        <div class="flex items-center gap-2">
+        <div class="flex items-center text-black gap-2">
           <NIcon class="text-2xl">
             <AlertCirle/>
           </NIcon>
           <div>
-            This website is still under development.
+            <span>This website is still under development.</span>
           </div>
         </div>
       </n-marquee>
     </n-alert>
   </div>
 </template>
+
+<style>
+.custom-close-button-alert .n-base-close {
+  color: black;
+}
+</style>

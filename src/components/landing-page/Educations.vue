69df68a37fe05da1fd61321679603e84ccdc6cba
aritlhq <aritlhq@gmail.com>
2025-06-19 15:56:39 +0700

    feat: add Educations component for displaying education timeline
    Implemented a new Vue component that renders education timeline entries from JSON data using Naive UI's timeline components. The component supports multiple education categories with customizable timeline items.

---
+++ src/components/landing-page/Educations.vue
@@ -0,0 +1,23 @@
+<script setup>
+import educationData from "../../assets/data/education-data.json"
+</script>
+
+<template>
+  <div
+      v-for="category in educationData.data"
+      :key="category.title"
+  >
+    <h2 class="text-md lg:text-2xl">{{ category.title }}</h2>
+    <NTimeline size="large">
+      <NTimelineItem
+          v-for="(item, index) in category.data"
+          :key="index"
+          :title="item.title"
+          :content="item.content"
+          :time="item.time"
+          :type="item.type || undefined"
+          :line-type="item['line-type'] || undefined"
+      />
+    </NTimeline>
+  </div>
+</template>

9ae63d15e72ca126b2d0710f0ee96729259828ed
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-08-16 21:11:18 +0700

    chore: Remove project files

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import './style.css'
-import 'virtual:uno.css'
-import App from './App.vue'
-import router from "./router/index.js";
-
-const app = createApp(App);
-app.use(router)
-app.mount('#app')
